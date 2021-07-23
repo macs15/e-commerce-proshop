1cd6d1df3a08fdedd21455b503d2949eb8e6d760
Maykell Carrillo <maykellcarrillo@gmail.com>
2021-07-23 18:33:45 -0300

    Corrige politica de cors

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ app.get("/", (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
   next()
 })
 app.use("/api/products", productRoutes)
