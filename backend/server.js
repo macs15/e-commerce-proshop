c359efeee06d598e9ca13d172239194eaf537a6d
Maykell Carrillo <maykellcarrillo@gmail.com>
2021-07-23 15:18:22 -0300

    Agrega nueva configuracion de cors

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,11 @@ app.get("/", (req, res) => {
   res.send("API is running..")
 })
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
+})
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
